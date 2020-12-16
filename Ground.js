d6e46c5feb523855ad6f819192d3e8b7004f8483
TeacherMeenu <74634943+TeacherMeenu@users.noreply.github.com>
2020-12-16 08:02:17 +0530

    done
    done

--- Ground.js
+++ Ground.js
@@ -1,23 +1,17 @@
-class Ground
-
-{
-
-    constructor(x,y,width,height)
-    {
-       var options ={isStatic:true} 
-       this.body=Bodies.rectangle(x,y,width,height,options);
-       World.add(world,this.body);
-       this.width=width;
-       this.height=height;
-
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
     }
-    display()
-    {
-        rectMode(CENTER);
-        fill("brown");
-        rect(this.body.position.x,this.body.position.y,this.width,this.height);
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);
     }
-
-
-    
-}
+  };
